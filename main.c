4bf70718488aabdf35e71a19d737d3ba0f83f35d
chogeunwoo <51676494+chogeunwoo@users.noreply.github.com>
2020-05-15 18:18:37 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
-#include<stdio.h>
-int main(){
-  printf("조근우\n");
-  printf("김민규\n");
-  printf("박주연\n");
-  printf("이상욱\n");
-  printf("이게우리야");
-  return 0;
+#include <stdio.h>
+
+int sum(num1,num2);
+
+void main(){
+  int calc=0;
+  int num1=0;
+  int num2=0;
+  printf("계산기 프로그램\n");
+  while(1){
+    printf("첫번째 수와 두번째 수를 입력해주세요.\n");
+    scanf_s("%d %d",&num1,&num2);
+    printf("원하시는 사칙연산을 선택하세요.\n");
+    printf("1)+ 2)- 3)* 4)/\n");
+    scanf_s("%d",&calc);
+    switch (calc) {
+      case 1:
+        sum(num1,num2);
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      default:
+        printf("위의 번호로 골라주세요\n");
+    }
+  }
+}
+void sum(num1,num2){
+  printf("%d + %d = %d\n", num1 ,num2 ,num1+num2);
 }
