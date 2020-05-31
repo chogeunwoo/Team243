3f95bea63a71a27222709912fe06639435b3acfa
Juyeon125 <juyeone125@gmail.com>
2020-05-31 15:51:42 +0900

    first commit

---
+++ oss_project/venv/app/routes.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+ 
+app = Flask(__name__)      
+
+@app.route('/')
+def main():
+  return render_template('main.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/services')
+def services():
+  return render_template('services.html')
+ 
+@app.route('/health_center')
+def health_center():
+  return render_template('health_center.html')
+ 
+@app.route('/login')
+def login():
+  return render_template('login.html')
+   
+
+
+if __name__ == '__main__':
+  app.debug = True
+  app.use_reloader=True
+  app.run(host='0.0.0.0', port=80) 
+  #port : 5000
