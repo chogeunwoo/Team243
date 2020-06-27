dd8890eb359f055e6870823834802325fc2ba8d3
MinGyu_kKim <minkyu19960308@gmail.com>
2020-06-27 21:07:26 +0900

    changemyinfo(부분수정, update 2차수정)

--- oss_project/venv/app/mysql_dao.py
+++ oss_project/venv/app/mysql_dao.py
@@ -44,6 +44,27 @@ def get_dbSelect_register(email,pw):
         else:
             return "fail"
 
+def get_dbInsert_changeMyinfo(name, email, pw, phone_num):
+    conn = connection.connection()
+    try:
+        sql = "SELECT email FROM test.os_member where email =" + "'" + email + "'"
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        row_num = cursor.rowcount
+    finally:
+        cursor.close()
+    if row_num > 0:
+        return "fail"
+    else:
+        try:
+            cursor = conn.cursor()
+            sql = "INSERT INTO os_member (name, email, pw, phone_num) VALUES (%s, %s, %s, %s);"
+            val = (name, email, pw, phone_num)
+            cursor.execute(sql,val)
+            conn.commit()
+        finally:
+            cursor.close()
+            return "true"
 
 def get_dbSelect_myinfo(email):
     conn = connection.connection()
